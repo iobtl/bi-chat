f3f94b493ff224afe9eb51aaab80a69025413965
iobtl <tedmundhtl@gmail.com>
2021-11-10 17:01:49 +0800

    Move route connection tests into `routes.rs`

--- src/routes.rs
+++ src/routes.rs
@@ -12,3 +12,51 @@ pub fn index(
 ) -> impl Filter<Extract = (warp::reply::Html<&'static str>,), Error = warp::Rejection> + Copy {
     warp::path::end().map(|| warp::reply::html(INDEX_HTML))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{html::INDEX_HTML, routes};
+    use futures::future;
+    use warp::test;
+
+    #[tokio::test]
+    async fn test_html_connection() {
+        let index = routes::index();
+
+        let response = test::request().reply(&index).await;
+
+        assert_eq!(response.status(), 200);
+        assert_eq!(response.body(), INDEX_HTML);
+    }
+
+    #[tokio::test]
+    async fn test_ws_connection() {
+        let chat = routes::chat().map(|ws: Ws, _| ws.on_upgrade(|_| future::ready(())));
+
+        test::ws()
+            .path("/chat/room1")
+            .handshake(chat)
+            .await
+            .expect("Handshake failed");
+
+        test::ws()
+            .path("/chat/room10")
+            .handshake(chat)
+            .await
+            .expect("Handshake failed");
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_ws_connection_panics() {
+        let chat = routes::chat().map(|ws: Ws, _| ws.on_upgrade(|_| future::ready(())));
+
+        // Should panic, since no room specified -- default should be 'public'
+        test::ws()
+            .path("/chat")
+            .handshake(chat)
+            .await
+            .expect("Handshake failed");
+    }
+}
